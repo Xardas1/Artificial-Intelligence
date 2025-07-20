a702d21cf4fc1660d590cb65afd566a165b0cf3f
Xardas1 <99086587+Xardas1@users.noreply.github.com>
2025-07-20 14:58:18 +0200

    Add files via upload

--- RL/learning-rl/Deep Q-Network (DQN) - FromScratch/dqn_agent.py
+++ RL/learning-rl/Deep Q-Network (DQN) - FromScratch/dqn_agent.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import gym
+import gymnasium as gym
+import ale_py
 import cv2
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
-env = gym.make("ALE/Pong-v5", render_mode="human")
-obs = env.reset()
 
+env = gym.make("ALE/Pong-v5", render_mode=None)
 
 class DQN(nn.Module):
     def __init__(self, in_channels, num_actions):
@@ -33,21 +33,10 @@ class DQN(nn.Module):
         x = self.fc1(x)
         return x
 
-
 input_size = 84
 num_classes = 6
-learning_rate = 0.0001
 batch_size = 32
 num_epochs = 100
-
-
-model = DQN(in_channels=4, num_actions=6)
-
-loss = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-
-
 
 def changing_to_grayscale(image):
     gray = 0.244 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
@@ -61,11 +50,10 @@ def image_preprocess(image):
 def stack_images(list_to_stack):
     stacked_images = np.stack((list_to_stack), axis=0)
     stacked_images = torch.tensor(stacked_images, dtype=torch.float32)
-    stacked_images = torch.unsqueeze(stacked_images, 0)
     return stacked_images
 
 def create_input_image(image, list_to_stack):
-    if len(list_to_stack) % 4 != 0 or len(list_to_stack) == 0:
+    if len(list_to_stack) < 4:
         list_to_stack.append(image)
     else:
         list_to_stack.pop(0)
@@ -76,15 +64,54 @@ def create_input_image(image, list_to_stack):
 
 def create_replay_memory(state_list, replay_memory, action, reward):
     if len(state_list) == 2:
-        replay_memory.extend([(state_list[0], torch.tensor(action), torch.tensor(reward), state_list[1])])
+        replay_memory.extend([
+            (state_list[0], torch.tensor(action), torch.tensor(reward), state_list[1])
+        ])
     return replay_memory
 
-def convert_to_tensor(batch):
-    states_st  = torch.tensor([np.stack(batch[i][0]) for i in range(len(batch))])
-    #action_at  = torch.tensor([np.stack(batch[j][1]) for j in range(len(batch))])
-    #reward_rt  = torch.tensor([np.stack(batch[k][2]) for k in range(len(batch))])
-    #states_st1 = torch.tensor([np.stack(batch[l][3]) for l in range(len(batch))])
+def extract_states(batch):
+    states_st  = torch.stack([batch[i][0] for i in range(len(batch))])
     return states_st
+
+def extract_actions(batch):
+    actions_at = torch.stack([batch[i][1] for i in range(len(batch))])
+    return actions_at
+
+def extract_rewards(batch):
+    reward_rt = torch.stack([batch[i][2] for i in range(len(batch))])
+    return reward_rt
+
+def extract_next_states(batch):
+    next_states_st1 = torch.stack([batch[i][3] for i in range(len(batch))])
+    return next_states_st1
+
+def create_correct_pred(y_pred,actions):
+    empty_list = []
+    for j, i in enumerate(actions):
+            empty_list.append(y_pred[j][i])
+    return empty_list
+
+def create_correct_pred_vectorized(y_pred, actions):
+    row_indices = np.arange(len(actions))
+    return y_pred[row_indices, actions]
+
+def calculate_y_target(reward, gamma, q_max):
+    y_target = reward + gamma * q_max
+    return y_target
+
+def e_greedy_policy(actions, epsilon):
+    if np.random.random() > epsilon:
+        actions = actions.detach().cpu().numpy()
+        action = np.argmax(actions)
+    else:
+        action = env.action_space.sample()
+    return action
+
+def epsilon_decay(epsilon, decay_rate):
+    epsilon = max(0.1, epsilon - decay_rate)
+    return epsilon
+
+
 #env.render()
 
 
--- RL/learning-rl/Deep Q-Network (DQN) - FromScratch/train.py
+++ RL/learning-rl/Deep Q-Network (DQN) - FromScratch/train.py
@@ -1,60 +1,102 @@
 import numpy as np
-import gym
+import gymnasium as gym
 import pdb
 from collections import deque
 import torch
-from dqn_agent import image_preprocess, stack_images, create_input_image, DQN, create_replay_memory, convert_to_tensor
+from dqn_agent import image_preprocess, stack_images, create_input_image, DQN, create_replay_memory, extract_states, extract_actions, create_correct_pred_vectorized, extract_rewards, extract_next_states, calculate_y_target, e_greedy_policy
+from dqn_agent import epsilon_decay
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch import optim
 from random import sample
 
 
-env = gym.make("ALE/Pong-v5", render_mode="human")
-obs = env.reset()
+env = gym.make("ALE/Pong-v5", render_mode=None)
 
 
 print("Action Space:", env.action_space)
 print("Possible actions:", list(range(env.action_space.n)))
 print("Observation Space:", env.observation_space)
 
+
+epochs = 5000000
+learning_rate = 0.0001
+gamma = 0.99
+epsilon_start = 1.0
+epsilon_final = 0.1
+decay_steps = 1000000
+epsilon = 1
+decay_rate = (epsilon_start - epsilon_final) / decay_steps
 list_to_stack = []
+initial_obs = []
 et = []
 st_prev_list = deque(maxlen=2)
 model = DQN(in_channels=4, num_actions=6)
+loss_fn = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+target_model = DQN(in_channels=4, num_actions=6)
+target_model.load_state_dict(model.state_dict())
 replay_memory = deque(maxlen=1000000)
 d = 0
+terminated = True
+truncated = True
+
+
 
 for i in range(1, 1000):
-    env.render()
-    action = env.action_space.sample()
+    if terminated or truncated:
+        obs, info = env.reset()
+     
+    image_preprocessed = image_preprocess(obs)
+
+    if i == 1:
+        for f in range(4):
+            stacked_images = create_input_image(image_preprocessed, list_to_stack)
+    else:
+        stacked_images = create_input_image(image_preprocessed, list_to_stack)
+
+    model_batch = torch.unsqueeze(stacked_images, 0)
+
+    actions = model(model_batch)
+
+    pdb.set_trace
+    action = e_greedy_policy(actions, epsilon)
+
+    pdb.set_trace()
+    epsilon = epsilon_decay(epsilon, decay_rate)
 
     obs, reward, terminated, truncated, info = env.step(action)
 
-    image_preprocessed= image_preprocess(obs)
-
-    #stacked_images = create_input_image(image_preprocessed, list_to_stack)
-    image_preprocessed= image_preprocess(obs)
-    stacked_images = create_input_image(image_preprocessed, list_to_stack)
-
-    if i >= 4:
-        st_prev_list.append(stacked_images)
-        
-
+    st_prev_list.append(stacked_images)
 
     replay_memory = create_replay_memory(st_prev_list, replay_memory, action, reward)
- 
+    
+    if i % 100 == 0:
+        target_model.load_state_dict(model.state_dict())
+
+
     if len(replay_memory) >= 32 and i % 4 == 0:
         batch = sample(replay_memory, 32)
-        #pdb.set_trace()
-        states_st = convert_to_tensor(batch)
-        #pdb.set_trace()
-        #y_pred = model(stacked_images)
-        #pdb.set_trace
+        model_states = extract_states(batch)
+        model_actions = extract_actions(batch)
+        model_rewards = extract_rewards(batch)
+        model_next_states = extract_next_states(batch)
+        y_pred_almost = model(model_states)
+        y_pred = create_correct_pred_vectorized(y_pred_almost, model_actions)
+        q_target = target_model(model_next_states)
+        q_max = q_target.max(dim=1).values
+        y_target = calculate_y_target(model_rewards, gamma, q_max)
+        loss = loss_fn(y_pred, y_target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    if terminated or truncated:
-        obs = env.reset()
+    if i % 10000 == 0:
+        torch.save(model.state_dict(), "pong_model.pth")
 
 
+    
+print("It Worked bitdh")
 
 
-
-#env.close()
-#print(env.action_space)
